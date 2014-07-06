ec5bc1311aadf86d43c84c3eb77934986f2a0c60
chris <labadmin@acute2.ersp.nyumc.org>
2014-07-06 19:35:20 -0400

    Adds system_definitions module.
    Add channel mapping calculation methods.

--- VoyeurSpikeGL/src/spike_py/spike_graph.py
+++ VoyeurSpikeGL/src/spike_py/spike_graph.py
@@ -7,9 +7,10 @@ import galry
 import numpy as np
 import scipy as sp
 from probe_definitions import probes
+from system_definition import systems
 from sgl_interface import SGLInterface, TestInterface
 from PyQt4 import QtCore, QtGui
-
+import time
 
 
 class SpikeGraph(QtGui.QWidget):
@@ -18,7 +19,6 @@ class SpikeGraph(QtGui.QWidget):
     '''
     
     trigger = QtCore.pyqtSignal()
-    
     def __init__(self, probe_type, refresh_period_ms = 1000, trigger_ch = None, acquisition_source = 'SpikeGL', **kwargs):
         self.refresh_period_ms = 1000
         super(SpikeGraph, self).__init__()
@@ -28,19 +28,28 @@ class SpikeGraph(QtGui.QWidget):
             self.acquisition_interface = SGLInterface()
         
         probe = probes[probe_type]
-        self.init_ui(probe)
+        system = system_definition[system]
+        self.acquisition_channels = self.combine_channels(probe,system)        
+        self.init_ui(probe,self.acquisition_channels)
         self.init_timer()
 
-        
-        
-        
     
-    def init_ui(self, probe):
+    
+    def combine_channels(self, probe): #defines a global channel list which will be used to pull data from acquisition source.
+        acquisition_channels = []
+        channel_idx = []
+        for idx, window in enumerate(probe.window_params):
+            channels = window['channels'].tolist()
+            acquisition_channels.extend(channels)
+        acquisition_channels.sort() 
+        return acquisition_channels
+    
+    def init_ui(self, probe, channel_mapping):
         
         window = QtGui.QGridLayout()
         self.graph_widgets = [] #list of graph widget objects that we will fill below.
         for window_params in probe.window_params:
-            temp_win = GraphWidget(window_params, self)
+            temp_win = GraphWidget(window_params,self.acquisition_channels, self)
             self.graph_widgets.append(temp_win)
         for widget in self.graph_widgets:
             position = widget.position
@@ -59,10 +68,6 @@ class SpikeGraph(QtGui.QWidget):
         # to global update signal that will be emitted after the acquisition data is loaded
         for widget in self.graph_widgets:
             self.trigger.connect(widget.update_graph_data)
-        
-        self.timer.setInterval(50000)
-        #start global timer for update.
-        self.timer.start()
 
         return
     
@@ -70,24 +75,59 @@ class SpikeGraph(QtGui.QWidget):
     def update(self):
         max_samples = self.acquisition_interface.acquisition_rate * self.refresh_period_ms/1000
 #         new_samples = self.acquisition_interface.get_next_data(max_samples = max_samples)
-        self.new_samples = 0.01* np.random.randn(64,self.acquisition_interface.acquisition_rate * self.refresh_period_ms/1000)
+        self.new_samples = 0.01* np.random.randn(64,self.acquisition_interface.acquisition_rate * self.refresh_period_ms/1000 *2)
         num_new_samples = self.new_samples.shape[1]
         
-        print 'trigger'
+#         print 'trigger'
+#         stime = time.time()
         self.trigger.emit()
+#         time_take = time.time() - stime
+#         print 'done '+ str(time_take)
         
         
 
 class GraphWidget(galry.GalryWidget):
+    pause = False
+    pause_label = 'Pause'
     
-    
-    def __init__(self, params, parent):
+    def __init__(self, params, global_channel_mapping, parent):
         self.parent_widget = parent
         self.channels = params['channels']
+        self.channel_mapping = self.calculate_channel_mapping(global_channel_mapping) 
         self.sites = params['site_numbers']
         self.position = params['grid_position']
         self.num_samples = 0
         super(GraphWidget,self).__init__()
+        
+    def calculate_channel_mapping(self, channel_mapping): #calculate where each channel is within the acquisition matrix. This will be used to pull data from the acquisition source matrix.
+        channel_map_array = np.array([], dtype = int)
+        for chan in self.channels:
+            chan_map = channel_mapping.index(chan)
+            channel_map_array = np.append(channel_map_array,chan_map)
+        return channel_map_array
+        
+    def mouseDoubleClickEvent(self,event):
+        print 'CME!'
+        self.popMenu = QtGui.QMenu()
+        anaction = self.popMenu.addAction(QtGui.QAction(self.pause_label, self,
+                statusTip="Cut the current selection's contents to the clipboard",
+                triggered=self.pause_update))
+        self.popMenu.addAction(QtGui.QAction("Reset View", self,
+                statusTip="Cut the current selection's contents to the clipboard",
+                triggered=self.interaction_manager.processors['navigation'].process_reset_event))
+
+        self.popMenu.exec_(event.globalPos())
+        
+        return
+    
+    def pause_update(self):
+        if self.pause:
+            self.pause = False
+            self.pause_label = 'Pause'
+        else:
+            self.pause = True
+            self.pause_label = 'Unpause'
+    
     
     def initialize(self):
         self.set_bindings(galry.PlotBindings)
@@ -99,20 +139,20 @@ class GraphWidget(galry.GalryWidget):
     
     @QtCore.pyqtSlot()
     def update_graph_data(self):
-        self.samples = self.parent_widget.new_samples[self.channels]
-        self.processed_samples = self.samples * self.interaction_manager.processors['navigation'].scalar + self.offsets
-        num_samples = self.samples.shape[1]
-#         if num_samples != self.num_samples:
-#             self.num_samples = num_samples
-#             self.x = np.tile(np.linspace(-1., 1., num_samples), (len(self.channels), 1))
-        position = np.vstack((self.x.flatten(), self.processed_samples.flatten())).T
-        self.paint_manager.set_data(position=position)
-        self.updateGL()
+        if not self.pause:
+            self.samples = self.parent_widget.new_samples[self.channel_mapping]
+            self.p_samples = self.samples * self.interaction_manager.processors['navigation'].scalar + self.offsets
+            #num_samples = self.samples.shape[1]
+    #         if num_samples != self.num_samples:
+    #             self.num_samples = num_samples
+    #             self.x = np.tile(np.linspace(-1., 1., num_samples), (len(self.channels), 1))
+    
+            self.paint_manager.set_data(position=np.vstack((self.x.ravel(), self.p_samples.ravel())).T)
+            self.updateGL()
 
     def zoom_y(self):
         self.processed_samples = self.samples * self.interaction_manager.processors['navigation'].scalar + self.offsets
-        position = np.vstack((self.x.flatten(), self.processed_samples.flatten())).T      
-        self.paint_manager.set_data(position=position)
+        self.paint_manager.set_data(position=np.vstack((self.x.ravel(), self.processed_samples.ravel())).T)
         self.updateGL()          
 
 
@@ -127,18 +167,18 @@ class GraphWidget(galry.GalryWidget):
     
 class MyPaintManager(galry.PlotPaintManager):
     def initialize(self):
-        self.parent.y = .01 * np.random.randn(self.parent.channels.size, 20833) + self.parent.calculate_offsets()
-        self.parent.x = np.tile(np.linspace(-1., 1., 20833), (self.parent.channels.size, 1))
-        self.add_visual(galry.PlotVisual, x=self.parent.x, y=self.parent.y, autocolor = True)
+        self.parent.y = .01 * np.random.randn(self.parent.channels.size, 20833*2) + self.parent.calculate_offsets()
+        self.parent.x = np.tile(np.linspace(-1., 1., 20833*2), (self.parent.channels.size, 1))
+        self.add_visual(galry.PlotVisual, x=self.parent.x, y=self.parent.y, autocolor = True,)
         
 class MyPlotInteractionManager(galry.DefaultInteractionManager):
-    def initialize_default(self, constrain_navigation=None,
+    def initialize_default(self, constrain_navigation=True,
         momentum=False,
         # normalization_viewbox=None
         ):
         super(MyPlotInteractionManager, self).initialize_default()
         self.add_processor(MyNavigationEventProcessor,
-            constrain_navigation=constrain_navigation, 
+            constrain_navigation=True, 
             # normalization_viewbox=normalization_viewbox,
             momentum=momentum,
             name='navigation')
@@ -162,7 +202,7 @@ class MyNavigationEventProcessor(galry.NavigationEventProcessor):
                 dx, dy = (min(dx, dy),) * 2
             else:
                 dx = dy = 0
-        #self.sx *= np.exp(dx)
+        self.sx *= np.exp(dx)
         self.scalar *= np.exp(dy) # this allows us to change the scaling on the y axis outside of the zoom function.        
         #self.sy *= np.exp(dy)
         
@@ -175,6 +215,7 @@ class MyNavigationEventProcessor(galry.NavigationEventProcessor):
         self.ty += -py * (1./self.syl - 1./self.sy)
         self.sxl = self.sx
         self.syl = self.sy
+        self.constrain_navigation = True
         self.parent.zoom_y()
         
                 
---
+++ VoyeurSpikeGL/src/spike_py/system_definitions.py
@@ -0,0 +1,39 @@
+class AbstractSystem(object):
+    '''
+    classdocs
+    '''
+
+    data = {}
+    window_params = []
+    
+    
+
+    
+    def build_window_params(self, acquisition_interface):
+        ''' window_n = {channels: (1,2,...n), position =(1,2,3,4) 'grid position'} 
+        window_1 = { }'''
+        
+        #this should return a windows dictionary.
+        
+        windows = {}
+        return windows
+    
+    def build_data(self):
+        data = {}
+        return data 
+    
+        
+        
+    
+    def __init__(self, acquisition_interface = 'SpikeGL'):
+        self.window_params = self.build_window_params(acquisition_interface)
+        self.data = self.build_data()        
+        num_sites = self.calculate_sites()
+        self.data['num_sites'] = num_sites
+        return
+    
+class Acute2System(AbstractSystem):
+    return
+
+
+system_definition = {'acute2' : Acute2System}
